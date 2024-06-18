57a1806afccc6170f7dd747d6d8df9c6d1f0333e
Huy Nguyen <huynguyendev02@gmail.com>
2024-06-18 13:45:40 +0700

    chore: Update Azure App Service deployment runtime to Python 3.8

--- commands.sh
+++ commands.sh
@@ -1,2 +1,2 @@
 echo  $WEBAPPNAME
-az webapp up --name $WEBAPPNAME --resource-group $RESOURCEGROUP --sku B1 --logs --runtime "PYTHON:3.8" --location southeastasia
+az webapp up --name $WEBAPPNAME --resource-group $RESOURCEGROUP --sku B1 --logs --runtime "PYTHON:3.8"
