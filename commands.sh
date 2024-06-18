2973a03839766d2b9ce5ec98fee752b87ade7bbc
Huy Nguyen <huynguyendev02@gmail.com>
2024-06-18 12:49:07 +0700

    chore: Update Azure App Service deployment variables

--- commands.sh
+++ commands.sh
@@ -1,1 +1,2 @@
-az webapp up --name $webAppName --resource-group $azureServiceConnectionId --sku B1 --logs --runtime "PYTHON:3.9"
+echo  $WEBAPPNAME
+az webapp up --name $WEBAPPNAME --resource-group $AZURESERVICECONNECTIONID --sku B1 --logs --runtime "PYTHON:3.9"
