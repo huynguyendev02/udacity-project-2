e9f20508302f996d9161a54e7071c8b18218026e
Huy Nguyen <huynguyendev02@gmail.com>
2024-06-18 13:19:15 +0700

    chore: Update Azure App Service deployment SKU to B1

--- commands.sh
+++ commands.sh
@@ -1,2 +1,2 @@
 echo  $WEBAPPNAME
-az webapp up --name $WEBAPPNAME --resource-group $RESOURCEGROUP --sku F1 --logs --runtime "PYTHON:3.9"
+az webapp up --name $WEBAPPNAME --resource-group $RESOURCEGROUP --sku B1 --logs --runtime "PYTHON:3.9"
